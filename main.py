cdb47d43510f5f4f428c6703d4e2cdecebb44e8a
Doniyorbek <ndoniyorbek1@gmail.com>
2023-02-11 09:27:16 +0500

    get method

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "<h1> Hello world! <h1>"
+# @app.route('/')
+# def hello_world():
+#     r = request.args
+#     name = r["name"]
+#     data = {
+#         'apple': 13,
+#         'banana': 15,
+#         'orange': 18
+#     }
+#     return {name: data.get(name, "not found")}
 
-@app.route('/salom')
-def salom():
-    return "<h1>salom qalaysiz! asd<h1>"
+@app.route('/get_sum')
+def get_summ():
+    r = request.args
+    a = int(r['a'])
+    b = int(r['b'])
+    return {"sum":a+b}
 
 
 if __name__ == '__main__':
